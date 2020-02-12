eab8d7adea668f142cea2ba2065bda53d865806b
pctechkid <omboyronnel@gmail.com>
2020-02-12 18:01:56 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "on-bt-download-complete=./on-complete.sh" >> aria2c.conf
+echo "on-download-complete=./on-complete.sh" >> aria2c.conf
 chmod +x on-complete.sh
 
 echo "rpc-secret=$ARIA2C_SECRET" >> aria2c.conf
