5ce2964876a49e1290ca50fb02f88b1478146de9
pctechkid <omboyronnel@gmail.com>
2020-02-12 15:30:01 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 if [[ -n $RCLONE_CONFIG && -n $RCLONE_DESTINATION ]]; then
 	echo "Rclone config detected"
-	echo -e "[DRIVE]\n$RCLONE_CONFIG" > rclone.conf
+	echo -e "[gdrive]\n$RCLONE_CONFIG" > rclone.conf
 	echo "on-bt-download-complete=./on-complete.sh" >> aria2c.conf
 	chmod +x on-complete.sh
 fi
